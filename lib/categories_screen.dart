42da5e9b8cc07cdeb96dc23619a0193f97e74216
Kartikey singh <kartikeysingh4474@gmail.com>
2021-10-09 19:56:38 +0530

    implemented navigation

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -9,10 +9,11 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView(
-        padding: EdgeInsets.all(15),
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
+                catData.id,
                 catData.title,
                 catData.color,
               ),
